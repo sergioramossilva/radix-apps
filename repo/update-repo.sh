a8c109e0b33920c238f73a0ea510a90476a699bf
Raul Dipeas <rauldipeas@mail.com>
2019-07-01 14:50:58 -0300

    GitLab-CI

--- repo/update-repo.sh
+++ repo/update-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update;sudo apt install -y reprepro dpkg-sig #xorriso fakeroot
+apt update;apt install -y reprepro dpkg-sig #xorriso fakeroot
 
 chmod +x -v\
  apps/sources-repo/hud-menu/opt/hud-menu/*\
